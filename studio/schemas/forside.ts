c987b2a317595e72615447ace81871d34c2667b5
Jørgen Jacobsen <jorgeja@gmail.com>
2024-01-28 15:23:42 +0100

    updated forside

--- studio/schemas/forside.ts
+++ studio/schemas/forside.ts
@@ -18,13 +18,17 @@ export default {
         {name: 'nameCreator', type: 'string', title: 'Navn på behandler'},
         {name: 'titleCreator', type: 'string', title: 'tittel på behandler'},
         {name: 'textActionButtons', type: 'string', title: 'Tekst over knapper til booking'},
-        {name: 'bookingUrl', type: 'url', title: 'Booking URL' },
+        {name: 'bookingUrl', type: 'url', title: 'Booking URL'},
         {name: 'bookingButtonName', type: 'string', title: 'Booking-knapp tittel'},
         {name: 'textAdditionalButtons', type: 'string', title: 'Tekst under bookingknapp'},
         {name: 'facebookBookingUrl', type: 'url', title: 'Facebook booking url'},
-        {name: 'facebookButtonText', type: 'url', title: 'Facebook booking knapp tittel'},
-        { name: 'phoneNumberBooking', type: 'string', title: 'Telefonnummer til booking' },
-        {name: 'phoneNumberBookingText', type: 'string', title: 'Tekst til telefonnummer booking-knapp'},
+        {name: 'facebookButtonText', type: 'string', title: 'Facebook booking knapp tittel'},
+        {name: 'phoneNumberBooking', type: 'string', title: 'Telefonnummer til booking'},
+        {
+          name: 'phoneNumberBookingText',
+          type: 'string',
+          title: 'Tekst til telefonnummer booking-knapp',
+        },
       ],
     },
     {
@@ -33,7 +37,8 @@ export default {
       type: 'array',
       of: [
         {
-          type: 'object', fields: [
+          type: 'object',
+          fields: [
             {
               title: 'Velg type',
               name: 'displaytype',
@@ -41,31 +46,39 @@ export default {
               options: {
                 list: [
                   {title: 'Høyrestilt bilde', value: 'imageRight'},
-                  { title: 'Bakgrunnsbilde', value: 'backgroundImage' },
-                  { title: 'Venstrestilt bilde', value: 'imageLeft' },
-                  { title: 'Høyrestilt Youtube', value: 'youtubeRight' },
-
+                  {title: 'Bakgrunnsbilde', value: 'backgroundImage'},
+                  {title: 'Venstrestilt bilde', value: 'imageLeft'},
+                  {title: 'Høyrestilt Youtube', value: 'youtubeRight'},
                 ], // <-- predefined values
-              }
+              },
             },
-            { name: 'title', type: 'string', title: 'Tittel' },
-            { name: 'photo', type: 'image', title: 'Bilde', hidden: ({ parent }: { parent: { displaytype: string } }) => parent.displaytype === "youtubeRight" },
-            { name: 'youtubeID', type: 'string', title: 'YouTube video ID', hidden: ({ parent }: { parent: {displaytype : string} }) => parent.displaytype !== "youtubeRight"},
+            {name: 'title', type: 'string', title: 'Tittel'},
+            {
+              name: 'photo',
+              type: 'image',
+              title: 'Bilde',
+              hidden: ({parent}: {parent: {displaytype: string}}) =>
+                parent.displaytype === 'youtubeRight',
+            },
+            {
+              name: 'youtubeID',
+              type: 'string',
+              title: 'YouTube video ID',
+              hidden: ({parent}: {parent: {displaytype: string}}) =>
+                parent.displaytype !== 'youtubeRight',
+            },
             {
               title: 'Tekst',
               name: 'content',
               type: 'array',
               of: [
-
                 {
                   type: 'block',
-                  styles: [
-                    {title: 'Normal', value: 'normal'},
-                  ]
+                  styles: [{title: 'Normal', value: 'normal'}],
                 },
-                  ]
-            }
-          ]
+              ],
+            },
+          ],
         },
       ],
     },
